6c456b989043dda7188c425e271d5f6b7778df45
navyap11 <ashwin.p.parmar@gmail.com>
2025-04-23 16:06:22 +0200

    r

---
+++ tuple operations.py
@@ -0,0 +1,18 @@
+tuple2= ("hi",True, 2, 6)
+print(tuple2)
+
+tuple2= (2,4,6,8,10)
+print(tuple2)
+
+tuple2= tuple2 +(9,)
+print(tuple2)
+
+tupplee=(76,84, 96, 47, 84)
+print(tupplee.count(84))
+
+tuple2= (2,5,7,1,4,3,6,8,9,11,10)
+print(tuple2)
+
+slice= tuple2[4:7]
+
+print(slice)
